abadde9bc889dba4302b9da5767d4cea027b8262
athulraj-mr <athulrajmr26@gmail.com>
2025-10-08 11:45:06 +0530

    saving-1

--- functions.js
+++ functions.js
@@ -5,6 +5,7 @@ async function loadItems() {
         const responds = await fetch('http://localhost:5000/items');
         itemsData = await responds.json();
         renderItems(itemsData);
+        wishlistBtn();
     } catch (error) {
         console.error("error loading", error);
     }
@@ -21,45 +22,44 @@ function renderItems(items) {
         li.classList.add("items-li");
 
         li.innerHTML = `
-            <li class="items-li">
-                <div class="items-li-con">
-                    <div class="items-li-out">
-                        <div class="items-li-out-img">
-                            <img width="300" height="200" src="${item.image}" alt="${item.name}">
-                        </div>
-                        <div class="items-li-outer">
-                            <a class="items-li-outer-a" href=""></a>
-                        </div>
-                        <div class="items-li-like">
-                            <div class="items-li-like-con">
-                                <div class="items-li-like-out">
-                                    <svg width="16" height="16" viewBox="0 0 512 512"><path xmlns="http://www.w3.org/2000/svg" fill="white" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path></svg>
-                                </div>
-                            </div>
-                        </div>
+            <div class="items-li-con">
+                <div class="items-li-out">
+                    <div class="items-li-out-img">
+                        <img width="300" height="200" src="${item.image}" alt="${item.name}">
                     </div>
-                    <div class="items-li-text">
-                        <a class="items-li-text-a" href="">
-                            <h2>${item.name}</h2>
-                        </a>
-                        <div class="items-li-text-out">
-                            <div class="items-li-text-outer">
-                                <span class="items-li-text-in"></span>
+                    <div class="items-li-outer">
+                        <a class="items-li-outer-a" href=""></a>
+                    </div>
+            
+                    <div class="items-li-like">
+                        <div class="items-li-like-con">
+                            <div class="items-li-like-out wishlist-btn" data-id="${item.id}">
+                                <svg width="16" height="16" viewBox="0 0 512 512"><path xmlns="http://www.w3.org/2000/svg" fill="white" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path></svg>
                             </div>
                         </div>
-                        <span class="items-li-text-out2">
-                            <del class="items-li-text-out2-del">
-                                <span class="items-li-text-out2-del-span">₹</span>
-                                ${item.original_price}
-                            </del>
-                            <ins class="items-li-text-out2-ins">
-                                <span class="items-li-text-out2-ins-span">₹</span>
-                                ${item.price}
-                            </ins>
-                        </span>
                     </div>
                 </div>
-            </li>
+                <div class="items-li-text">
+                    <a class="items-li-text-a" href="">
+                        <h2>${item.name}</h2>
+                    </a>
+                    <div class="items-li-text-out">
+                        <div class="items-li-text-outer">
+                            <span class="items-li-text-in"></span>
+                        </div>
+                    </div>
+                    <span class="items-li-text-out2">
+                        <del class="items-li-text-out2-del">
+                            <span class="items-li-text-out2-del-span">₹</span>
+                            ${item.original_price}
+                        </del>
+                        <ins class="items-li-text-out2-ins">
+                            <span class="items-li-text-out2-ins-span">₹</span>
+                            ${item.price}
+                        </ins>
+                    </span>
+                </div>
+            </div>
         `;
 
         container.appendChild(li);
@@ -80,6 +80,7 @@ document.getElementById("sort-select").addEventListener("change", (e)=> {
     }
 
     renderItems(sorted);
+    wishlistBtn();
 });
 
 
@@ -105,6 +106,7 @@ function filterPrice() {
     });
 
     renderItems(filtered);
+    wishlistBtn();
 }
 
 [minRange, maxRange].forEach(input=> {
@@ -117,5 +119,48 @@ function filterPrice() {
         filterPrice();
     });
 });
+
+//wishlist
+
+function  getWishlist() {
+    return JSON.parse(localStorage.getItem("wishlist")) || [];
+}
+
+function saveWishlist(wishlist) {
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
+}
+
+function wishlistBtn() {
+
+    const btn = document.querySelectorAll(".wishlist-btn");
+    const wishlist = getWishlist();
+
+    btn.forEach(b=> {
+        const id = b.dataset.id;
+
+        b.classList.toggle("active",wishlist.includes(id));
+
+        b.replaceWith(b.cloneNode(true));
+    });
+
+    const button = document.querySelectorAll(".wishlist-btn");
+
+    button.forEach(btn=> {
+        const id = btn.dataset.id;
+
+        btn.addEventListener("click", () => {
+            let current = getWishlist();
+
+            if(current.includes(id)) {
+                current = current.filter(itmId=> itmId !== id);
+                btn.classList.remove("active");
+            } else {
+                current.push(id);
+                btn.classList.add("active");
+            }
+            saveWishlist(current);
+        });
+    })
+}
 
 loadItems();
