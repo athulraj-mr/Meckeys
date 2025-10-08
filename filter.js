da98243ea5ed81308268e0ace367a317f245591b
athulraj-mr <athulrajmr26@gmail.com>
2025-10-08 21:23:02 +0530

    filter by stock added

--- filter.js
+++ filter.js
@@ -56,29 +56,3 @@ function lowest(cla) {
     })
     return studenstLow;
 }
-
-
-/*
-
-                                    <li class="s-stock-ul-li">
-
-                                                    <div class="s-stock-ul-li-con">
-                                                        <label class="s-stock-ul-li-out" for="">
-                                                            <input class="s-stock-ul-li-outer stock-filter" value="in" type="checkbox">
-                                                            <svg class="wc-block-components-checkbox__mark" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 20"><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg>
-                                                            <span class="s-stock-ul-li-out-span">In stock</span>
-                                                        </label>
-                                                    </div>
-                                                </li>
-                                                <li class="s-stock-ul-li">
-                                                    <div class="s-stock-ul-li-con">
-                                                        <label class="s-stock-ul-li-out" for="">
-                                                            <input class="s-stock-ul-li-outer stock-filter" value="out" type="checkbox">
-                                                            <svg class="wc-block-components-checkbox__mark" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 20"><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg>
-                                                            <span class="s-stock-ul-li-out-span">Out of stock</span>
-                                                        </label>
-                                                    </div>
-
-
-                                                    
-                                                </li>*/
